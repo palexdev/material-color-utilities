61d36417d3278313f91637c1a39d9fb37146af96
palexdev <alessandro.parisi406@gmail.com>
2026-07-03 13:30:54 +0200

    :wrench: Forgot module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module material.color.utilities {
+    exports io.github.palexdev.mcu;
+    exports io.github.palexdev.mcu.enums;
+
+    // Vendor
+    exports io.github.palexdev.mcu.vendor.blend;
+    exports io.github.palexdev.mcu.vendor.contrast;
+    exports io.github.palexdev.mcu.vendor.dislike;
+    exports io.github.palexdev.mcu.vendor.dynamiccolor;
+    exports io.github.palexdev.mcu.vendor.hct;
+    exports io.github.palexdev.mcu.vendor.palettes;
+    exports io.github.palexdev.mcu.vendor.quantize;
+    exports io.github.palexdev.mcu.vendor.scheme;
+    exports io.github.palexdev.mcu.vendor.score;
+    exports io.github.palexdev.mcu.vendor.temperature;
+    exports io.github.palexdev.mcu.vendor.utils;
+}
